ef7c2e43bbdf0f2a1a2745a068a8c30b2f3d1e1d
TaukkiKaheli <TaukkiKaheli@users.noreply.github.com>
2017-09-19 13:52:37 +0300

    Add files via upload

--- jquery_functions.js
+++ jquery_functions.js
@@ -1,3 +1,39 @@
 $(".playerbtn").click(function(){
-    $(".player").toggle();
+    $(".albumcover").show();
+    $(".home").hide();
+    $(".browse").hide();
+    $(".playlist").hide();
+    $(".bio").hide();
+});
+
+$(".homebtn").click(function(){
+    $(".albumcover").hide();
+    $(".home").show();
+    $(".browse").hide();
+    $(".playlist").hide();
+    $(".bio").hide();
+});
+
+$(".browsebtn").click(function(){
+    $(".albumcover").hide();
+    $(".home").hide();
+    $(".browse").show();
+    $(".playlist").hide();
+    $(".bio").hide();
+});
+
+$(".playlistbtn").click(function(){
+    $(".albumcover").hide();
+    $(".home").hide();
+    $(".browse").hide();
+    $(".playlist").show();
+    $(".bio").hide();
+});
+
+$(".biobtn").click(function(){
+    $(".albumcover").hide();
+    $(".home").hide();
+    $(".browse").hide();
+    $(".playlist").hide();
+    $(".bio").show();
 });
