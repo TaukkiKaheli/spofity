9e544293ed0e3502d98a3447d216f21b2b07131a
TaukkiKaheli <TaukkiKaheli@users.noreply.github.com>
2017-09-21 12:18:49 +0300

    Add files via upload

--- js/jquery_functions.js
+++ js/jquery_functions.js
@@ -1,49 +1,76 @@
 $(".playerbtn").click(function(){
+    $(this).css("background-color", "red");
     $(".albumcover").show();
+    $(".homebtn").css("background-color", "black");
     $(".home").hide();
+    $(".browsebtn").css("background-color", "black");
     $(".browse").hide();
+    $(".playlistbtn").css("background-color", "black");
     $(".playlist").hide();
+    $(".biobtn").css("background-color", "black");
     $(".bio").hide();
 });
 
 $(".homebtn").click(function(){
+    $(".playerbtn").css("background-color", "black");
     $(".albumcover").hide();
+    $(this).css("background-color", "red");
     $(".home").show();
+    $(".browsebtn").css("background-color", "black");
     $(".browse").hide();
+    $(".playlistbtn").css("background-color", "black");
     $(".playlist").hide();
+    $(".biobtn").css("background-color", "black");
     $(".bio").hide();
 });
 
 $(".browsebtn").click(function(){
+    $(".playerbtn").css("background-color", "black");
     $(".albumcover").hide();
+    $(".homebtn").css("background-color", "black");
     $(".home").hide();
+    $(this).css("background-color", "red");
     $(".browse").show();
+    $(".playlistbtn").css("background-color", "black");
     $(".playlist").hide();
+    $(".biobtn").css("background-color", "black");
     $(".bio").hide();
 });
 
 $(".playlistbtn").click(function(){
+    $(".playerbtn").css("background-color", "black");
     $(".albumcover").hide();
+    $(".homebtn").css("background-color", "black");
     $(".home").hide();
+    $(".browsebtn").css("background-color", "black");
     $(".browse").hide();
+    $(this).css("background-color", "red");
     $(".playlist").show();
+    $(".biobtn").css("background-color", "black");
     $(".bio").hide();
 });
 
 $(".biobtn").click(function(){
+    $(".playerbtn").css("background-color", "black");
     $(".albumcover").hide();
+    $(".homebtn").css("background-color", "black");
     $(".home").hide();
+    $(".browsebtn").css("background-color", "black");
     $(".browse").hide();
+    $(".playlistbtn").css("background-color", "black");
     $(".playlist").hide();
+    $(this).css("background-color", "red");
     $(".bio").show();
 });
-    // ---- BROWSE EP ALASVEDOT ---- //
-    $("#alas1").click(function(){
-        $(".ep1piilo").slideToggle();
-        $(".ep2piilo").slideUp();
-    });
-    $("#alas2").click(function(){
-        $(".ep2piilo").slideToggle();
-        $(".ep1piilo").slideUp();
-    });
-    // ---- BROWSE EP ALASVEDOT ---- //
+
+// ---- BROWSE EP ALASVEDOT ---- //
+$("#alas1").click(function(){
+    $(".ep1piilo").slideToggle();
+    $(".ep2piilo").slideUp();
+});
+
+$("#alas2").click(function(){
+    $(".ep2piilo").slideToggle();
+    $(".ep1piilo").slideUp();
+});
+// ---- BROWSE EP ALASVEDOT ---- //
