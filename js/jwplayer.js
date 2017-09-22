8d2f77bf43189ac209335c99557f26498a4ce27b
tomipohj <tomipohj@metropolia.fi>
2017-09-22 10:13:05 +0300

    Create jwplayer.js

---
+++ js/jwplayer.js
@@ -0,0 +1,34 @@
+            var playerInstance = jwplayer("myElement");
+            playerInstance.setup({
+            hlshtml: true,
+            
+            height:300,
+            width: 300,
+            
+            playlist: [{
+                file:"Cyranoi - Abiogenesis.mp3",
+                image:"exist.jpg",
+                title:"Cyranoi - Abiogenesis"
+                
+            },
+            {
+                file:"Cyranoi - Pioneers.mp3",
+                image:"exist.jpg"
+            },
+            {
+                file:"Cyranoi - Lucid Dream.mp3",
+                image:"challengerdeep.jpg"
+            },
+                       
+            {
+                file:"Cyranoi - Flesh And Mind (ft. Sithu Aye).mp3"
+            },
+            {
+                file:"Cyranoi - Avarice.mp3"
+            },
+            {
+                file:"Cyranoi - Event Horizon (ft. Niklas Turunen).mp3"
+            },
+                
+                ]
+            });
